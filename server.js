c6026d51d63944bd3f977fda326be5b891ece1f6
root <root@ip-172-31-63-225.ec2.internal>
2017-01-09 20:46:21 +0000

    Added pic to response

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ var app = express();
 // Route for / - default page
 app.get('/', function (req, res) 
 {
-	res.setHeader('content-type', 'application/json');
-	res.send(JSON.stringify(req.headers,null,4));
+	res.setHeader('Content-Type', 'text/html');
+
+	formattedResponse = "<html><body><h1>What Just Happened?</h1><br>";
+	formattedResponse += "<img src=\"https://apiaccessmgmt.galvin.ninja/images/header-based-auth-diagram.png\"/>";
+	formattedResponse += "<br><h1>Headers Received Are Below</h1><br>";
+	formattedResponse += "<pre>";
+	formattedResponse += JSON.stringify(req.headers,null,4);
+	formattedResponse += "</pre>";
+	formattedResponse += "</body></html";
+
+	res.send(formattedResponse);
 })
 
 httpPort = 8082;
